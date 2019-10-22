caeee3de3293a20a594c63d04a4a85687f3210fb
em-Qwerty <arrecis59@gmail.com>
2019-10-22 07:42:53 -0600

    plantilla navbottonbar

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,26 +1,61 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
 
-  const HomePage({Key key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _page = 0;
+  GlobalKey _bottomNavigationKey = GlobalKey();
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home page'),
-      ),
-      body: Center( 
-        child: SingleChildScrollView(
-          child: Container( 
-            child: Column( 
+        bottomNavigationBar: CurvedNavigationBar(
+          key: _bottomNavigationKey,
+          index: 0,
+          height: 50.0,
+          items: <Widget>[
+            Icon(Icons.history, size: 30),
+            Icon(Icons.list, size: 30),
+            Icon(Icons.compare_arrows, size: 30),
+            Icon(Icons.call_split, size: 30),
+            Icon(Icons.perm_identity, size: 30),
+          ],
+          color: Colors.white,
+          buttonBackgroundColor: Colors.white,
+          backgroundColor: Colors.lightBlueAccent,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 300),
+          onTap: (index) {
+            setState(() {
+              _page = index;
+            });
+          },
+        ),
+        body: Container(
+          color: Colors.lightBlueAccent,
+          child: Center(
+            child: Column(
               children: <Widget>[
-                Text("I'm the home page")
+                // Text(_page.toString(), textScaleFactor: 10.0),
+                // RaisedButton(
+                //   child: Text('Go To Page of index 1'),
+                //   onPressed: () {
+                //     final CurvedNavigationBarState navBarState =
+                //         _bottomNavigationKey.currentState;
+                //     navBarState.setPage(1);
+                //   },
+                // )
               ],
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
