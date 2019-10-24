f12401e13c531804ef13b8409da3fd227727599f
em-Qwerty <arrecis59@gmail.com>
2019-10-23 20:47:56 -0600

    vista carga de imagen corregida (analisis-page)

--- lib/src/pages/analisys_page.dart
+++ lib/src/pages/analisys_page.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
 import 'package:flutter/material.dart';
 
 class AnalisysPage extends StatefulWidget {
@@ -8,10 +11,131 @@ class AnalisysPage extends StatefulWidget {
 }
 
 class _AnalisysPageState extends State<AnalisysPage> {
+
+  File foto;
+  String status = '';
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text('Analisys Page'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink.shade200,
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: <Widget>[
+                _botonTomar(),
+                _botonSeleccionar(),
+              ],
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+            _mostrarImagen(),
+            _botonAnalisis()
+            ],
+          ),
+        ),
+      ),
     );
   }
+
+  Widget _botonTomar() {
+    return Container(
+      child: IconButton(
+        icon: Icon(Icons.camera),
+        color: Colors.white,
+        onPressed: _tomarImagen,
+      ),
+    );
+  }
+
+  Widget _botonSeleccionar() {
+    return Container(
+      child: IconButton(
+        icon: Icon(Icons.photo_size_select_actual),
+        color: Colors.white,
+        onPressed: _seleccionarImagen,
+      ),
+    );
+  }
+
+  Widget _mostrarImagen() {
+
+    double _width = MediaQuery.of(context).size.width;
+
+    if (foto != null) {
+      return Container( 
+        margin: EdgeInsets.only(top: 10.0),
+        child: Image.file(
+          File(foto.path),
+          width: _width,
+          height: 300.0,
+          fit: BoxFit.contain,
+        ),
+      );
+    } else {
+      return Container( 
+        margin: EdgeInsets.only(top: 20.0),
+        child: Image(
+          image: AssetImage('assets/no-image.png'),
+          height: 300.0,
+          fit: BoxFit.cover
+        ),
+      );
+    }
+  }
+
+
+  Widget _botonAnalisis(){
+    return Container( 
+      margin: EdgeInsets.symmetric(vertical: 15.0),
+      child: ButtonTheme( 
+        minWidth: MediaQuery.of(context).size.width / 2,
+        height: 40.0,
+        child: RaisedButton( 
+          // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4.0)),
+          color: Colors.pink.shade200,
+          textColor: Colors.white,
+          child: Text( 
+            'Iniciar análisis'
+          ),
+          onPressed: (){},
+        ),
+      ),
+    );
+  }
+
+  _setStatus(String message) {
+    setState(() {
+      status = message;
+    });
+  }
+
+  // Widget _status() {
+  //   return Text(status);
+  // }
+
+  _procesarImagen(ImageSource origin) async {
+    foto = await ImagePicker.pickImage(source: origin);
+    setState(() {});
+  }
+
+  _seleccionarImagen() async {
+    _procesarImagen(ImageSource.gallery);
+    _setStatus('');
+  }
+
+  _tomarImagen() async {
+    _procesarImagen(ImageSource.camera);
+    _setStatus('');
+  }
+
+
+
 }
