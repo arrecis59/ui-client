08d03b5a97177e48b6ae3eafcf061f835f2e0fdd
em-Qwerty <arrecis59@gmail.com>
2019-10-27 19:47:15 -0600

    cambio de colores bar

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -18,8 +18,8 @@ class _HomePageState extends State<HomePage> {
   Color backgroudColor = Colors.white;
 
   List<NavigationItem> items = [
-    NavigationItem(Icon(Icons.home), Text('Home'), Colors.blue.shade300),
-    NavigationItem(Icon(Icons.insert_chart), Text('Análisis'), Colors.green.shade300),
+    NavigationItem(Icon(Icons.home), Text('Home'), Color.fromRGBO(47, 181, 121, 0.7)),
+    NavigationItem(Icon(Icons.insert_chart), Text('Análisis'), Colors.blue.shade300,),
     NavigationItem(Icon(Icons.history), Text('Historial'), Colors.deepOrange.shade300),
     NavigationItem(Icon(Icons.person_outline), Text('Cuenta'), Colors.teal.shade200),
   ];
