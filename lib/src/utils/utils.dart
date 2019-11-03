ca78b0930162b46f15ce80ba6b7170bc8e95d677
em-Qwerty <arrecis59@gmail.com>
2019-11-02 21:36:11 -0600

    nuevos mensajes de error en utilities

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -7,6 +7,14 @@ void mostrarAlerta(BuildContext context, String message, String type) {
 
   }else if(message == 'INVALID_PASSWORD'){
     message = '¡Contraseña incorrecta!';
+  }else if(message == 'EMAIL_EXISTS'){
+    message = '¡La cuenta ya existe!';
+  }
+  else if(message == 'WEAK_PASSWORD : Password should be at least 6 characters'){
+    message = '¡La contraseña debe ser de almenos 6 caracteres!';
+  }
+  else if(message == 'TOO_MANY_ATTEMPTS_TRY_LATER : Too many unsuccessful login attempts.  Please include reCaptcha verification or try again later'){
+    message = '¡Demasiados intentos de sesión incorrectos, por favor intentelo más tarde!';
   }
 
   showDialog(
