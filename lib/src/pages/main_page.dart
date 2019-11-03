607d253568c1c17e330b3482ab20c469ff4fef88
em-Qwerty <arrecis59@gmail.com>
2019-11-02 21:07:56 -0600

    main page terminada

--- lib/src/pages/main_page.dart
+++ lib/src/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart' as UrlLauncher;
 
 class MainPage extends StatefulWidget {
   MainPage({Key key}) : super(key: key);
@@ -9,23 +10,29 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
 
-  String xatoma = "son protuberancias firmes que puede ser color rosa o amarrillo que pueden aparecer en los brasos, tronco o piernas" +
+  String xatoma = "Son protuberancias firmes que puede ser color rosa o amarrillo que pueden aparecer en los brasos, tronco o piernas" +
   ". La presencia de esta lesión puede ser asociada a niveles anormales de lipidos o grasas en la sangre";
   
-  String sweet = "se caracteriza por una constelación de síntomas entre ellos fiebre, neutrofilia, lesiones cutaneas dolorosas (pápulas)" +
+  String sweet = "Se caracteriza por una constelación de síntomas entre ellos fiebre, neutrofilia, lesiones cutaneas dolorosas (pápulas)" +
   ". El sindrome puede observarse en tres situaciones clásica, asociada al cancer y inducida por fármacos";
+  
+  String iderma = "Instituto especializado en dermatología clínica y cirugia de piel, a precios bajos de atención.";
 
-  String iderma = "centro especializado en dermatología clínica y cirugía con bajos precios de atención. " +
-  "Ubicado en Calzada San Juan 32-11 Zona 7";
+  String tinea = "Es una infeccion fungicida de la piel que afecta la capa a su capa más exterior, llamada extra corneo. " +
+  "la tinea negra produce un parche negro o marron en la piel, esta se puede confundir con decoloraciones de la piel, pero" +
+  " en realidad es una afección benigna.";
+
+  String urticaria = "Son ronchas rojizas que pueden producir picazón en la piel. Suele ser causada por una reacción alérgica a"+
+  "la comida, medicamentos y otros agentes irritantes.";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
       appBar: AppBar( 
         title: Text( 
-          'Lo más relevante',
+          'Información importante',
           style: TextStyle( 
-            fontSize: 17.0,
+            fontSize: 15.0,
             color: Colors.white
           ),
         ),
@@ -49,6 +56,9 @@ class _MainPageState extends State<MainPage> {
               _institucion(),
               _noticia(),
               _noticia2(),
+              _noticia3(),
+              _noticia4(),
+              _noticia5(),
             ],
           ),
         ),
@@ -90,7 +100,7 @@ class _MainPageState extends State<MainPage> {
                       xatoma,
                       textAlign: TextAlign.justify,
                         style: TextStyle( 
-                          fontSize: 14.0,
+                          fontSize: 15.0,
                           fontWeight: FontWeight.w300,
                           color: Color.fromRGBO(63, 66, 66, 0.6),
                         ),
@@ -135,7 +145,142 @@ class _MainPageState extends State<MainPage> {
                       sweet,
                       textAlign: TextAlign.justify,
                         style: TextStyle( 
-                          fontSize: 14.0,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w300,
+                          color: Color.fromRGBO(63, 66, 66, 0.6),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+      );
+  }
+
+  Widget _noticia3(){
+    return Container( 
+              
+              width: MediaQuery.of(context).size.width / 1.2,
+              padding: EdgeInsets.all(30.0),
+              margin: EdgeInsets.symmetric(vertical: 20.0),
+              decoration: BoxDecoration( 
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow( 
+                    color: Color.fromRGBO(185, 194, 194, 0.56),
+                    blurRadius: 20.0,
+                    offset: Offset(0, 10.0)
+                  )
+                ]
+              ),
+              child: Column( 
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text(
+                      '¿Qué es la tinea negra?',
+                       textAlign: TextAlign.center,
+                        style: TextStyle( 
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      SizedBox(height: 15.0,),
+                      Text(
+                      tinea,
+                      textAlign: TextAlign.justify,
+                        style: TextStyle( 
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w300,
+                          color: Color.fromRGBO(63, 66, 66, 0.6),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+      );
+  }
+
+  Widget _noticia4(){
+    return Container( 
+              
+              width: MediaQuery.of(context).size.width / 1.2,
+              padding: EdgeInsets.all(30.0),
+              margin: EdgeInsets.symmetric(vertical: 20.0),
+              decoration: BoxDecoration( 
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow( 
+                    color: Color.fromRGBO(185, 194, 194, 0.56),
+                    blurRadius: 20.0,
+                    offset: Offset(0, 10.0)
+                  )
+                ]
+              ),
+              child: Column( 
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text(
+                      '¿Qué es la pitiriasis versicolor?',
+                       textAlign: TextAlign.center,
+                        style: TextStyle( 
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      SizedBox(height: 15.0,),
+                      Text(
+                      tinea,
+                      textAlign: TextAlign.justify,
+                        style: TextStyle( 
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w300,
+                          color: Color.fromRGBO(63, 66, 66, 0.6),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+      );
+  }
+
+  Widget _noticia5(){
+    return Container( 
+              
+              width: MediaQuery.of(context).size.width / 1.2,
+              padding: EdgeInsets.all(30.0),
+              margin: EdgeInsets.symmetric(vertical: 20.0),
+              decoration: BoxDecoration( 
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow( 
+                    color: Color.fromRGBO(185, 194, 194, 0.56),
+                    blurRadius: 20.0,
+                    offset: Offset(0, 10.0)
+                  )
+                ]
+              ),
+              child: Column( 
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text(
+                      '¿Qué es la urticaria?',
+                       textAlign: TextAlign.center,
+                        style: TextStyle( 
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      SizedBox(height: 15.0,),
+                      Text(
+                      urticaria,
+                      textAlign: TextAlign.justify,
+                        style: TextStyle( 
+                          fontSize: 15.0,
                           fontWeight: FontWeight.w300,
                           color: Color.fromRGBO(63, 66, 66, 0.6),
                         ),
@@ -184,11 +329,38 @@ class _MainPageState extends State<MainPage> {
                       iderma,
                       textAlign: TextAlign.justify,
                         style: TextStyle( 
-                          fontSize: 14.0,
+                          fontSize: 15.0,
                           fontWeight: FontWeight.w300,
                           color: Color.fromRGBO(63, 66, 66, 0.6),
                         ),
                       ),
+                      SizedBox(height: 20.0,),
+                      Row( 
+                        children: <Widget>[
+                          Icon(Icons.location_on),
+                          Flexible(
+                            child: Column( 
+                              children: <Widget>[
+                                Text(' Calzada San Juan 32-11 Zona 7', textAlign: TextAlign.center,),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      FlatButton( 
+                        child: Row( 
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.call),
+                            Text(
+                            ' +502 2208 9494'
+                            )
+                          ],
+                        ),
+                        onPressed: (){
+                          UrlLauncher.launch("tel:+502 2208 9494");
+                        },
+                      )
                     ],
                   )
                 ],
