8487af28ae49d393a993f237379a1ae63d87239a
em-Qwerty <arrecis59@gmail.com>
2019-11-16 18:54:48 -0600

    metrica ubicacion añadida a metricas provider

--- lib/src/providers/metricas_provider.dart
+++ lib/src/providers/metricas_provider.dart
@@ -6,6 +6,7 @@ class MetricasProvider{
 
   final urlApi = 'https://app-enfermedades.herokuapp.com';
 
+  //METRICA ENFERMEDADES
   Future<dynamic> obtenerMetricaEnfermedades() async{
 
     final url = '$urlApi/generarMetricaEnfermedades';
@@ -22,7 +23,25 @@ class MetricasProvider{
     final formatData = json.decode(resp.body);
 
     return formatData;
+  }
 
+  //METRICA UBICACION
+  Future<dynamic> obtenerMetricaUbicacion() async{
+
+    final url = '$urlApi/generarMetricaUbicacionEnfermedades';
+
+    final resp = await http.get(url, headers: {
+       HttpHeaders.contentTypeHeader: "application/json"
+    });
+
+    if(resp.statusCode !=200){
+      print('Error al generar metrica de ubicacion');
+      return null;
+    }
+
+    final formatData = json.decode(resp.body);
+
+    return formatData;
 
   }
 
